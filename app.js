18814f3bd74437ac3825c3858dc09870239887d3
Pableras90 <62385345+Pableras90@users.noreply.github.com>
2023-02-07 20:49:58 +0100

    Update app.js
    Finish commit

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 const reservas = [
     {
         tipoHabitacion: "standard",
+        desayuno: false,
         pax: 1,
         noches: 3
     },
     {
         tipoHabitacion: "standard",
+        desayuno: false,
         pax: 1,
         noches: 4
     },
     {
         tipoHabitacion: "suite",
+        desayuno: true,
         pax: 2,
         noches: 1
     }
 ];
 
-//Caso 1
-//"standar"=100; "suite=150";
-//+1 Pax=40€
-// iva=subtotal + 21%;
 
 
-
-
-class CalculoHabitacion {
-
+class CalculoGeneral {
     constructor() {
         this._reserva = [];
         this._subtotal = 0;
@@ -33,12 +29,8 @@ class CalculoHabitacion {
         this._total = 0;
 
     }
-
     calculaSubtotal() {
-        this._subtotal = reservas.reduce((acumulado, lineaReserva) => acumulado + (lineaReserva.noches * (lineaReserva.pax * (lineaReserva.tipoHabitacion === "standard" ? 100 : 150))), 0);
-    }
-    calculaTotal() {
-        return this._total = this._subtotal * this._iva;
+
     }
 
     get total() {
@@ -52,11 +44,60 @@ class CalculoHabitacion {
         this.calculaSubtotal();
         this.calculaTotal();
     }
+    calculaTotal() {
+        return this._total = this._subtotal * this._iva;
+    }
+    calculaPax(personas) {
+        return (personas > 1 ? (personas - 1) * 40 : 0)
+    }
+    calculaDesayuno(desayuno, personas, noches) {
+        return desayuno ? (15 * personas * noches) : 0;
+    }
+}
 
 
+class CalculoHabitacion extends CalculoGeneral {
+
+    constructor() {
+        super();
+    }
+
+    calculaSubtotal() {
+        this._subtotal = reservas.reduce((acumulado, lineaReserva) =>
+            acumulado + lineaReserva.noches * (
+                (this.calculaPax(lineaReserva.pax))
+                + this.calculaDesayuno(lineaReserva.desayuno, lineaReserva.pax, lineaReserva.noches)
+                + (lineaReserva.tipoHabitacion === "standard" ? 100 + (this.calculaPax(lineaReserva.pax)) : 150)), 0);
+    }
 }
 
 const precioHotel = new CalculoHabitacion();
 
 precioHotel.reservas = reservas;
-console.log("subtotal:", precioHotel.total)
+console.log("Subtotal:", precioHotel.subtotal);
+console.log("Total:", precioHotel.total);
+
+
+class TourOperador extends CalculoGeneral {
+    constructor() {
+        super();
+        this._descuento = 0.15;
+    }
+
+    calculaSubtotal() {
+        this._subtotal = reservas.reduce((acumulado, lineaReserva) =>
+            acumulado + lineaReserva.noches * (
+             (this.calculaPax(lineaReserva.pax))
+             + this.calculaDesayuno(lineaReserva.desayuno, lineaReserva.pax, lineaReserva.noches)
+             + 100), 0);
+    }
+
+    calculaTotal() {
+        return this._total = super.calculaTotal() * (1 - this._descuento);
+    }
+}
+
+const tourOperador = new TourOperador();
+tourOperador.reservas = reservas;
+console.log("Subtotal Tour:", tourOperador.subtotal);
+console.log("Total Tour:", tourOperador.total);
